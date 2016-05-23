8ab55dcaa6c5ad7ccdb91581ff986480110f4569
Geoff <geoffreyps@gmail.com>
2016-05-23 16:50:44 -0500

    Revise calc_value_delta to use Map functions instead of Access protocols on TaxValues struct

--- lib/delta_funs.ex
+++ lib/delta_funs.ex
@@ -2,15 +2,18 @@ defmodule TaxValue.DeltaFuns do
 	@moduledoc"""
 	Functions for calculating the annual delta and value delta for a TaxValue.
 	"""
-	@spec calc_value_delta(TaxValue) :: TaxValue
-	def calc_value_delta(tax_value) do
-		%{ tax_value | value_delta: calc_annual_delta(tax_value[:gross_value], tax_value[:taxed_value]) }
+	@spec calc_value_delta(%TaxValue{}) :: %TaxValue{}
+	def calc_value_delta(tax_value) when is_map(tax_value) do
+		gross_value = Map.get(tax_value, :gross_value)
+		taxed_value = Map.get(tax_value, :taxed_value)
+
+		%{ tax_value | 
+			value_delta: calc_value_delta(gross_value, taxed_value)
+		}
 	end
 
 	@spec calc_value_delta(number, number) :: number
-	defp calc_value_delta(gross_value, taxed_value), do: gross_value - taxed_value
-
-
-	def 
-
+	defp calc_value_delta(gross_value, taxed_value) when is_number(gross_value) and is_number(taxed_value) do
+	 gross_value - taxed_value
+	end
 end
