677bf20ba611a3648b6f24e637f32803b639bfde
Geoff <geoffreyps@gmail.com>
2016-05-24 16:55:04 -0500

    Add test for find_delta_funs

--- test/delta_funs_test.exs
+++ test/delta_funs_test.exs
@@ -32,7 +32,19 @@ defmodule DeltaFunsTest do
     assert(ad_2012 == 0 && ad_2013 == -2_000 && ad_2014 == 1_000)
   end
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "function find_value_deltas returns a Building with Value Deltas for each year" do
+    building = %Building{
+      tax_values: %{
+        2012 => %TaxValue{gross_value: 10_000, taxed_value: 8_000, value_delta: nil, annual_delta: 0},
+        2013 => %TaxValue{gross_value: 12_000, taxed_value: 6_000, value_delta: nil, annual_delta: 0},
+        2014 => %TaxValue{gross_value: 11_000, taxed_value: 7_000, value_delta: nil, annual_delta: 0}
+      }
+    }
+
+    %{2012 => %TaxValue{value_delta: vd_2012}, 
+      2013 => %TaxValue{value_delta: vd_2013}, 
+      2014 => %TaxValue{value_delta: vd_2014}} = Map.get(TaxValue.DeltaFuns.find_value_deltas(building), :tax_values)
+      
+    assert(vd_2012 == 2_000 && vd_2013 == 6_000 && vd_2014 == 4_000)
   end
 end
