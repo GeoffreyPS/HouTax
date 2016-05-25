d55053d45a3ff5c7b46f1d41e3f98fc0b14c00b6
Geoff <geoffreyps@gmail.com>
2016-05-25 16:47:00 -0500

    Refactor tests: use setup_all and reduce verbosity

--- test/delta_funs_test.exs
+++ test/delta_funs_test.exs
@@ -2,63 +2,50 @@ defmodule DeltaFunsTest do
   use ExUnit.Case
   doctest CsvParse
 
-  test "function calc_value_delta returns a taxed value's gross_value minus its taxed_value" do
-  	 tax_vals = %{	2012 => %TaxValue{gross_value: 10_000, taxed_value: 8_000, value_delta: nil, annual_delta: 0},
-  									2013 => %TaxValue{gross_value: 12_000, taxed_value: 6_000, value_delta: nil, annual_delta: 0},
-  									2014 => %TaxValue{gross_value: 11_000, taxed_value: 7_000, value_delta: nil, annual_delta: 0}
-  							}
 
+  setup_all do 
+    {:ok, building: %Building{
+                      tax_values: %{
+                        2012 => %TaxValue{gross_value: 10_000, taxed_value: 8_000, value_delta: nil, annual_delta: 0},
+                        2013 => %TaxValue{gross_value: 12_000, taxed_value: 6_000, value_delta: nil, annual_delta: 0},
+                        2014 => %TaxValue{gross_value: 11_000, taxed_value: 7_000, value_delta: nil, annual_delta: 0}
+                      }
+    }}
+  end
 
+  test "meta test", meta do
+    assert %Building{} = meta[:building] 
+  end
+
+  test "function calc_value_delta returns a taxed value's gross_value minus its taxed_value", meta do
+    tax_vals = Map.get(meta[:building], :tax_values)
   	year = Map.keys(tax_vals) |> Enum.shuffle |> List.first
   	new_val = TaxValue.DeltaFuns.calc_value_delta(Map.get(tax_vals, year))
   	assert (Map.get(new_val, :value_delta)) == (Map.get(tax_vals[year], :gross_value) - Map.get(tax_vals[year], :taxed_value))
   end
 
-
-
-  test "function find_annual_deltas returns a Building with Annual Deltas" do
-    building = %Building{ 
-      tax_values: %{
-        2012 => %TaxValue{taxed_value: 8_000, annual_delta: 0},
-        2013 => %TaxValue{taxed_value: 6_000, annual_delta: 0},
-        2014 => %TaxValue{taxed_value: 7_000, annual_delta: 0}
-      }
-    }
+  test "function find_annual_deltas returns a Building with Annual Deltas", meta do
     
     %{2012 => %TaxValue{annual_delta: ad_2012}, 
       2013 => %TaxValue{annual_delta: ad_2013}, 
-      2014 => %TaxValue{annual_delta: ad_2014}} = Map.get(TaxValue.DeltaFuns.find_annual_deltas(building), :tax_values)
+      2014 => %TaxValue{annual_delta: ad_2014}} = Map.get(TaxValue.DeltaFuns.find_annual_deltas(meta[:building]), :tax_values)
 
     assert(ad_2012 == 0 && ad_2013 == -2_000 && ad_2014 == 1_000)
   end
 
-  test "function find_value_deltas returns a Building with Value Deltas for each year" do
-    building = %Building{
-      tax_values: %{
-        2012 => %TaxValue{gross_value: 10_000, taxed_value: 8_000, value_delta: nil, annual_delta: 0},
-        2013 => %TaxValue{gross_value: 12_000, taxed_value: 6_000, value_delta: nil, annual_delta: 0},
-        2014 => %TaxValue{gross_value: 11_000, taxed_value: 7_000, value_delta: nil, annual_delta: 0}
-      }
-    }
+  test "function find_value_deltas returns a Building with Value Deltas for each year", meta do
 
     %{2012 => %TaxValue{value_delta: vd_2012}, 
       2013 => %TaxValue{value_delta: vd_2013}, 
-      2014 => %TaxValue{value_delta: vd_2014}} = Map.get(TaxValue.DeltaFuns.find_value_deltas(building), :tax_values)
+      2014 => %TaxValue{value_delta: vd_2014}} = Map.get(TaxValue.DeltaFuns.find_value_deltas(meta[:building]), :tax_values)
       
     assert(vd_2012 == 2_000 && vd_2013 == 6_000 && vd_2014 == 4_000)
   end
 
-  test "function find_deltas returns Building with both Value and Annual Deltas for each tax year" do
-    building = %Building{
-      tax_values: %{
-        2012 => %TaxValue{gross_value: 10_000, taxed_value: 8_000, value_delta: nil, annual_delta: 0},
-        2013 => %TaxValue{gross_value: 12_000, taxed_value: 6_000, value_delta: nil, annual_delta: 0},
-        2014 => %TaxValue{gross_value: 11_000, taxed_value: 7_000, value_delta: nil, annual_delta: 0}
-      }
-    }
+  test "function find_deltas returns Building with both Value and Annual Deltas for each tax year", meta do
     %{2012 => %TaxValue{value_delta: vd_2012, annual_delta: ad_2012}, 
       2013 => %TaxValue{value_delta: vd_2013, annual_delta: ad_2013}, 
-      2014 => %TaxValue{value_delta: vd_2014, annual_delta: ad_2014}} = Map.get(TaxValue.DeltaFuns.find_deltas(building), :tax_values)
+      2014 => %TaxValue{value_delta: vd_2014, annual_delta: ad_2014}} = Map.get(TaxValue.DeltaFuns.find_deltas(meta[:building]), :tax_values)
 
       assert((vd_2012 == 2_000 && vd_2013 == 6_000 && vd_2014 == 4_000) && (ad_2012 == 0 && ad_2013 == -2_000 && ad_2014 == 1_000))
   end
