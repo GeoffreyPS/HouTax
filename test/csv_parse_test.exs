fe6f015818512bd258dd711b8a01d5e13de4b02d
Geoff <geoffreyps@gmail.com>
2016-05-26 11:00:34 -0500

    Include setup for receiving CSV rows; include test case for to_tax_value

--- test/csv_parse_test.exs
+++ test/csv_parse_test.exs
@@ -2,7 +2,19 @@ defmodule CsvParseTest do
   use ExUnit.Case
   doctest CsvParse
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  setup_all do
+  	csv = "./data/2012.csv"
+  	row = csv
+  				|> Path.expand(__DIR__)
+  				|> File.stream!
+  				|> CSV.decode(headers: true)
+  				|> Enum.take_random(1)
+  				|> List.first
+  	{:ok, row: row}
+  end
+
+  test "to_tax_value returns a valid TaxValue", meta do
+  	tax_value = CsvParse.to_tax_value(meta[:row])
+    refute Map.get(tax_value, :gross_value) == nil
   end
 end
