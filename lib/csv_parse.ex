ba70ef3805b5f6a2a689e6295a2ef27c6ef9a645
Geoff <geoffreyps@gmail.com>
2016-05-26 15:49:26 -0500

    Ensure Buildings and TaxValues maintain data contract when created; add composted function returning building with values

--- lib/csv_parse.ex
+++ lib/csv_parse.ex
@@ -8,17 +8,22 @@ defmodule CsvParse do
 	"""
 	@spec to_tax_value(map) :: %TaxValue{}
 	def to_tax_value(%{"Property_Class" => building_type, "GROSSVAL" => gross_value, "TAXVALUE" => taxed_value}) do
- 		tax_value = %TaxValue{building_type: building_type, gross_value: gross_value, taxed_value: taxed_value}
+ 		tax_value = %TaxValue{building_type: building_type, gross_value: String.to_integer(gross_value), taxed_value: String.to_integer(taxed_value)}
 	end
-
 
 	@doc"""
 	Accepts a CSV row (with headers) and returns a Building
 	"""
 	@spec to_building(map) :: %Building{}
 	def to_building(%{"CAN" => id, "PNUMBER" => street_number, "PSTRNAME" => street_name, "ZIPCODE" => zip}) do
-		building = %Building{id: id, street_number: street_number, street_name: street_name,
-												city: "Houston", zip: zip}
-
+		building = %Building{id: String.to_integer(id), street_number: String.to_integer(street_number), street_name: street_name,
+												city: "Houston", zip: String.to_integer(zip)}
 	end
+
+	@spec building_with_value(map, integer) :: %Building{}
+	def building_with_value(row, year) do
+		building = to_building(row)
+		%Building{building | tax_values: %{year => to_tax_value(row)} }
+	end
+
 end
