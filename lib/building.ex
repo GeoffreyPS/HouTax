bbb61263c694257619a2417d8a0e14da863ca431
GeoffreyPS <geoffreyps@gmail.com>
2016-05-30 17:51:49 -0500

    Add convenience methods for reading data contained within data types

--- lib/building.ex
+++ lib/building.ex
@@ -9,4 +9,49 @@ defmodule Building do
 
 	@type t :: %Building{id: String.t, street_number: integer, street_name: String.t, 
 											city: String.t, zip: integer, tax_values: %{integer => TaxValue}}
+
+
+	@doc"""
+	Convenience function for retreiving a Building's id
+	"""
+	@spec id(%Building{}) :: integer
+	def id(%Building{id: id}), do: id
+
+	@doc"""
+	Convenience function for retreiving a Building's street number
+	"""
+	@spec street_number(%Building{}) :: integer
+	def street_number(%Building{street_number: street_number}), do: street_number
+
+	@doc"""
+	Convenience function for retreiving a Building's street name
+	"""
+	@spec street_name(%Building{}) :: String.t
+	def street_name(%Building{street_name: street_name}), do: street_name
+
+	@doc"""
+	Convenience function for retreiving a Building's city
+	"""
+	@spec city(%Building{}) :: String.t
+	def city(%Building{city: city}), do: city
+
+	@doc"""
+	Convenience function for retreiving a Building's zip code
+	"""
+	@spec zip(%Building{}) :: Integer
+	def zip(%Building{zip: zip}), do: zip
+
+	@doc"""
+	Convenience function for retreiving a Building's tax_values.
+	Returns a Map of Tax Values with Integer keys.
+	"""
+	@spec tax_values(%Building{}) :: %{integer => %TaxValue{}}
+	def tax_values(%Building{tax_values: tax_values}), do: tax_values
+
+	@doc"""
+	Convenience function for retreiving a Building's individual Tax Value
+	"""
+	@spec tax_value(%Building{}, integer) :: %TaxValue{}
+	def tax_value(%Building{tax_values: tax_values}, year), do: Map.get(tax_values, year)
+
 end
--- lib/tax_value.ex
+++ lib/tax_value.ex
@@ -10,4 +10,34 @@ defmodule TaxValue do
 
 	@type t :: %TaxValue{	building_type: String.t, gross_value: number,
 												taxed_value: number, value_delta: number, annual_delta: number } 
+
+	@doc"""
+	Convenience function for accessing a TaxValue's building type
+	"""
+	@spec building_type(%TaxValue{}) :: String.t
+	def building_type(%TaxValue{building_type: building_type}), do: building_type
+
+	@doc"""
+	Convenience function for accessing a TaxValue's building type
+	"""
+	@spec gross_value(%TaxValue{}) :: number
+	def gross_value(%TaxValue{gross_value: gross_value}), do: gross_value
+
+	@doc"""
+	Convenience function for accessing a TaxValue's building type
+	"""
+	@spec taxed_value(%TaxValue{}) :: number
+	def taxed_value(%TaxValue{taxed_value: taxed_value}), do: taxed_value
+
+	@doc"""
+	Convenience function for accessing a TaxValue's building type
+	"""
+	@spec value_delta(%TaxValue{}) :: number
+	def value_delta(%TaxValue{value_delta: value_delta}), do: value_delta
+
+	@doc"""
+	Convenience function for accessing a TaxValue's building type
+	"""
+	@spec annual_delta(%TaxValue{}) :: number
+	def annual_delta(%TaxValue{annual_delta: annual_delta}), do: annual_delta
 end
