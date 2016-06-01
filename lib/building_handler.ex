d77ca62c8d0a1c37b5b6fda9012fec4d5abce34e
GeoffreyPS <geoffreyps@gmail.com>
2016-05-31 19:44:36 -0500

    Implement init/1 callback

--- lib/building_handler.ex
+++ lib/building_handler.ex
@@ -1,5 +1,32 @@
 defmodule BuildingHandler do
 	use GenServer
+	import Building
+
+	defmodule State do
+		defstruct[:building]
+	end
+
+
+
+
+# Callbacks
+	def init([dispatcher, config]) when is_pid(dispatcher) do
+		state = %State{}
+		init[config, state]
+	end
+
+	def init([], state) do
+		{:ok, state}
+	end
+
+	def init([row: row, year: year | rest], state) do
+		%{state | building: Deserializer.building_with_value(csv_row, year)}
+		init(rest, state)
+	end
+
+	def init([_ | rest], state) do
+		init(rest, state)
+	end
 
 
 end
