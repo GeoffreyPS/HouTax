cd97d2359318c1e9c893e39142389da8f9788624
Geoff <geoffreyps@gmail.com>
2016-06-17 10:47:59 -0500

    Add interface functions for processing csvs and reporting on single buildings

--- lib/hou_tax.ex
+++ lib/hou_tax.ex
@@ -6,4 +6,20 @@ defmodule HouTax do
 		HouTax.Supervisor.start_link
 	end
 	
+	def process(csv) when is_binary(csv) do
+		csv
+		|> Path.expand(__DIR__)
+		|> HouTax.Reader.process
+	end
+
+	def process(csvs) when is_list(csvs) do
+		for csv <- csvs, do: process(csv)
+	end
+
+	def report(building_id) do
+		building_id
+		|> Building.Cache.server_process
+		|> Building.Server.report
+	end
+
 end
