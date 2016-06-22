7ba3c2a9d0a21b5e1fc68ed7d8ddaca4b8198a88
Geoff <geoffreyps@gmail.com>
2016-06-22 16:37:11 -0500

    Implement awaiting function to prevent writer from writing too soon; refactor run function

--- lib/houtax_cli.ex
+++ lib/houtax_cli.ex
@@ -5,22 +5,70 @@ defmodule HouTax.CLI do
 		run(parsed_args)
 	end
 
-
 	def run([{:path, path} | _rest]) do
 		File.cd(path)
 		{:ok, files} = File.ls
-		csvs = Enum.filter(files, &(Regex.match?(~r/.*\.csv$/, &1)))
-		pathed_csvs = for csv <- csvs, do: "." <> path <> "/" <> csv
-		Enum.map(pathed_csvs, &Path.expand(&1, __DIR__))
-		|> IO.inspect
-		|> HouTax.process
-		IO.inspect(Building.Server.report("0010160000011"))
-		IO.inspect(:pg2.get_members(:buildings))
-		IO.inspect(length :pg2.get_members(:buildings))
-		Process.sleep(4_000)
-		IO.inspect(length :pg2.get_members(:buildings))
+
+		csvs = files
+					|> filter_csvs
+					|> get_paths
+
+		csvs
+			|> HouTax.process
+
+		await(:buildings, csvs)
 		HouTax.write_all
-		IO.inspect(length :pg2.get_members(:buildings))
+	end
+
+	def filter_csvs(files) do
+		files
+		|> Enum.filter(&(Regex.match?(~r/.*\.csv$/, &1)))
+	end
+
+	def get_paths(csvs) do
+		{:ok, new_path} = File.cwd
+		paths = for csv <- csvs, do: new_path <> "/" <> csv
+		Enum.map(paths, &Path.expand(&1, __DIR__))
+	end
+
+	def await(group, files) do
+			unless group_size(group) >= at_least(files)
+				do Process.sleep(50)
+				await(group, files)
+			else 
+				Process.sleep(500)
+		end
+	end
+
+	defp group_size(group) do
+		:pg2.get_members(group)
+		|> length
+	end
+
+	defp at_least(files) do
+		[filename: filename, size: _size] = largest_file files
+		
+		filename 
+		|> File.stream! 
+		|> Enum.to_list 
+		|> length
+	end
+
+	defp largest_file(files) do
+		Enum.max_by(files, &size(&1))
+		|> file_name_size
+	end
+
+	defp size(file) do
+		{:ok, stat} = File.stat(file)	
+		%{size: size} = stat	
+		size
+	end
+
+	defp file_name_size(file) do
+		{:ok, stat} = File.stat(file)	
+		%{size: size} = stat	
+		[filename: file, size: size]
 	end
 
 end
